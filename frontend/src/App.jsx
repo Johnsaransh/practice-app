95b0c4c4eefc245d5c648f06b8bf355b54370146
Johnsaransh <saranshj2003@gmail.com>
2025-11-25 18:15:58 -0700

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,13 @@
 import './App.css'
 
+
 function App() {
 
   return (
-    <>
-   <Text />
-   <Text />
+   <>
+
    </>
-  )
-
+  );
 }
 
-function Text() {
-  return (
-    <div>
-      <p>hello world</p>
-    </div>
-    );
-  
-}
-export default App
+export default App;
