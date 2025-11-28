27d9c59ac1dcc5f7a35c34dbae33fe113db96255
Johnsaransh <saranshj2003@gmail.com>
2025-11-28 00:29:06 -0700

    changes

---
+++ frontend/src/component/NavBar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Movie App</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to="/" className="nav-link"></Link>
+        </nav>
+    );
+}
